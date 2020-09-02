517a31b49a924832d8467c808588b78dcff6593d
Ghassan Nasr <ghassan.nasr@gmail.com>
2020-09-02 01:40:58 -0400

    Completed Muffin annotations and controllers and tested successfully with Postman.

--- src/main/java/com/github/curriculeon/controllers/MuffinController.java
+++ src/main/java/com/github/curriculeon/controllers/MuffinController.java
@@ -4,31 +4,44 @@ import com.github.curriculeon.models.Muffin;
 import com.github.curriculeon.services.MuffinService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+@Controller
+@RequestMapping("/muffin-controller")
 public class MuffinController {
+
     private MuffinService service;
 
     public MuffinController(MuffinService service) {
         this.service = service;
     }
 
+    @RequestMapping(value = "/all-muffins", method = RequestMethod.GET)
     public ResponseEntity<Iterable<Muffin>> index() {
         return new ResponseEntity<>(service.index(), HttpStatus.OK);
     }
 
-    public ResponseEntity<Muffin> show(Long id) {
+    @RequestMapping(value = "/show-muffin/{id}", method = RequestMethod.GET)
+    public ResponseEntity<Muffin> show(@PathVariable Long id) {
         return new ResponseEntity<>(service.show(id), HttpStatus.OK);
     }
 
-    public ResponseEntity<Muffin> create(Muffin baker) {
+    @RequestMapping(value = "/create-muffin", method = RequestMethod.POST)
+    public ResponseEntity<Muffin> create(@RequestBody Muffin baker) {
         return new ResponseEntity<>(service.create(baker), HttpStatus.CREATED);
     }
 
-    public ResponseEntity<Muffin> update(Long id, Muffin baker) {
+    @RequestMapping(value = "/update-muffin/{id}", method = RequestMethod.PUT)
+    public ResponseEntity<Muffin> update(@PathVariable Long id, @RequestBody Muffin baker) {
         return new ResponseEntity<>(service.update(id, baker), HttpStatus.OK);
     }
 
-    public ResponseEntity<Boolean> destroy(Long id) {
+    @RequestMapping(value = "/destroy-muffin/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<Boolean> destroy(@PathVariable Long id) {
         return new ResponseEntity<>(service.delete(id), HttpStatus.OK);
     }
 }
