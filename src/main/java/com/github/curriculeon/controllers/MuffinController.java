5b565d5bc8c7f6b8f62589f857816e5116531f51
Emmanuel Orubele <blontf@gmail.com>
2020-09-05 17:15:57 -0400

    completed the assignment

--- src/main/java/com/github/curriculeon/controllers/MuffinController.java
+++ src/main/java/com/github/curriculeon/controllers/MuffinController.java
@@ -2,33 +2,43 @@ package com.github.curriculeon.controllers;
 
 import com.github.curriculeon.models.Muffin;
 import com.github.curriculeon.services.MuffinService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
+@Controller
+@RequestMapping(value = "/muffin")
 public class MuffinController {
     private MuffinService service;
 
+    @Autowired
     public MuffinController(MuffinService service) {
         this.service = service;
     }
 
+    @GetMapping(value = "/read/all")
     public ResponseEntity<Iterable<Muffin>> index() {
         return new ResponseEntity<>(service.index(), HttpStatus.OK);
     }
 
-    public ResponseEntity<Muffin> show(Long id) {
+    @GetMapping(value = "/read/{id}")
+    public ResponseEntity<Muffin> show(@PathVariable Long id) {
         return new ResponseEntity<>(service.show(id), HttpStatus.OK);
     }
 
-    public ResponseEntity<Muffin> create(Muffin baker) {
+    @PostMapping(value = "/create")
+    public ResponseEntity<Muffin> create(@RequestBody Muffin baker) {
         return new ResponseEntity<>(service.create(baker), HttpStatus.CREATED);
     }
-
-    public ResponseEntity<Muffin> update(Long id, Muffin baker) {
+    @PutMapping(value = "/update/{id}")
+    public ResponseEntity<Muffin> update(@PathVariable Long id,@RequestBody Muffin baker) {
         return new ResponseEntity<>(service.update(id, baker), HttpStatus.OK);
     }
 
-    public ResponseEntity<Boolean> destroy(Long id) {
+    @DeleteMapping(value = "/delete/{id}")
+    public ResponseEntity<Boolean> destroy(@PathVariable Long id) {
         return new ResponseEntity<>(service.delete(id), HttpStatus.OK);
     }
 }
--- src/main/java/com/github/curriculeon/repositories/BakerRepository.java
+++ src/main/java/com/github/curriculeon/repositories/BakerRepository.java
@@ -2,6 +2,8 @@ package com.github.curriculeon.repositories;
 
 import com.github.curriculeon.models.Baker;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BakerRepository extends CrudRepository<Baker, Long> {
 }
